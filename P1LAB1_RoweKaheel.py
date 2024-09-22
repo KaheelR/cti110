e607640f8f8ab5da6940d196cc32274be19b76f5
KaheelR <rowek4671@student.faytechcc.edu>
2024-09-22 13:24:26 -0400

    Add files via upload
    First Submission

---
+++ P1LAB1_RoweKaheel.py
@@ -0,0 +1,11 @@
+#Kaheel Rowe
+#09/22/2024
+#P1LAB1
+#I will be creating a program that will recieve input from the user and display the output
+
+first_name = str(input())
+last_name = str(input())
+
+print(f"Enter your first name: {first_name}")
+print(f"Enter your last name: {last_name}")
+print(f"Hello, {first_name} {last_name}! Welcome to CTI-110")
