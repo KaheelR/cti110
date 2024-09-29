0ce28675721fc9358ecd38010588ff1686396c04
KaheelR <rowek4671@student.faytechcc.edu>
2024-09-29 15:00:11 -0400

    Add files via upload

---
+++ P1HW1_RoweKaheel.py
@@ -0,0 +1,19 @@
+# Kaheel Rowe
+# 09/29/2024
+# P1HW1
+# This assignment will calculate exponents and it will calculate addition and subtraction
+print("----Calculating Exponents----")
+
+
+base = int(input("Enter an integer as the base value: "))
+exponent = int(input("Enter an integer as the exponent: "))
+
+print(f"{base} raised to the power of {exponent} is {base**exponent} !!")
+
+print("----Addition and Subtraction----")
+
+num1 = int(input("Enter a starting integer: "))
+num2 = int(input("Enter an integer to add: "))
+num3 = int(input("Enter an integer to subtract: "))
+
+print(f"{num1} + {num2} - {num3} is equal to {num1+num2-num3}")
